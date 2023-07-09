b036f7bb64cc0d7bdb624155d8f9568db92d1a15
Fa-C-Shus <facshus@gmail.com>
2023-07-09 15:06:47 -0500

    feat: add dart

---
+++ 2022/adventdart/lib/adventdart.dart
@@ -0,0 +1,10 @@
+/// adventdart, 2022 Advent of code in Dart
+///
+/// ```sh
+/// # activate adventdart
+/// dart pub global activate adventdart
+///
+/// # see usage
+/// aoc --help
+/// ```
+library adventdart;
