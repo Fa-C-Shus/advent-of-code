1b3a1ced8d17cc71d1020ff07fb126f556ce4b3f
Fa-C-Shus <facshus@gmail.com>
2023-12-14 19:01:06 -0600

    feat: aoc-2023-day-13-II

--- 2023/ocaml/puzzle_13_1.ml
+++ 2023/ocaml/puzzle_13_1.ml
@@ -1,31 +1,13 @@
 open Core
 
-let get_data = 
-  let rec get_data' all_data running_data =
-    match In_channel.input_line In_channel.stdin with
-    | None -> 
-      let grid = Array.of_list (List.rev running_data) in
-      all_data @ [grid]
-    | Some "" -> 
-      let grid = Array.of_list (List.rev running_data) in
-      get_data' (all_data @ [grid]) []
-    | Some line -> 
-      let row = Array.of_list (String.to_list line) in
-      get_data' all_data (row :: running_data)
-  in
-  get_data' [] []
-;;
+let read_grids filename =
+  let data = In_channel.read_all filename in
+  List.map ~f:(fun grid -> 
+    List.map ~f:String.to_list (String.split grid ~on:'\n')) (Str.split (Str.regexp "\n\n") data)
 
-let print_grid grid =
-  Array.iter grid ~f:(fun row ->
-    Array.iter row ~f:(fun c -> printf "%c" c);
-    printf "\n"
-  )
-;;
-
-let calc_badness grid part2 axis =
+let calc_badness (grid: char list list) part2 axis =
   let rows = List.length grid in
-  let cols = List.hd_exn grid |> String.length in
+  let cols = List.hd_exn grid |> List.length in
   let calc_axis_badness pos delta_pos get_other_pos =
     let badness = ref 0 in
     for delta = 0 to delta_pos - 1 do
@@ -34,52 +16,52 @@ let calc_badness grid part2 axis =
       if pos1 >= 0 && pos2 < delta_pos then
         for other = 0 to get_other_pos - 1 do
           let char1, char2 = match axis with
-            | `Vertical -> List.nth_exn grid other |> fun row -> row.[pos1], row.[pos2]
-            | `Horizontal -> grid.(pos1).[other], grid.(pos2).[other]
+            | `Vertical -> 
+              let row = List.nth_exn grid other in
+              (List.nth_exn row pos1, List.nth_exn row pos2)
+            | `Horizontal -> 
+              let row1 = List.nth_exn grid pos1 in
+              let row2 = List.nth_exn grid pos2 in
+              (List.nth_exn row1 other, List.nth_exn row2 other)
           in
-          if char1 <> char2 then incr badness
+          if (not (Char.equal char1 char2)) then incr badness
         done
     done;
     !badness
   in
   let total = ref 0 in
   for i = 0 to (match axis with `Vertical -> cols - 1 | `Horizontal -> rows - 1) - 1 do
-    let badness = match axis with
+    let badness = 
+      match axis with
       | `Vertical -> calc_axis_badness i cols rows
       | `Horizontal -> calc_axis_badness i rows cols
     in
     if badness = (if part2 then 1 else 0) then
-      total := !total + (if axis = `Horizontal then 100 else 1) * (i + 1)
+      total := !total + 
+      match axis with 
+      | `Horizontal -> 100 * (i + 1)
+      | `Vertical -> 1 * (i + 1)
+(*         
+        (if axis = `Horizontal then 100 else 1) * (i + 1) *)
   done;
   !total
 
-let rec sum_symmetry sum grids =
-  match grids with
-  | [] -> sum
-  | grid :: grids' -> 
-    printf "grid(cols, rows): (%d, %d)\n" (Array.length grid.(0)) (Array.length grid);
-    let symmetry = (calc_badness grid false `Vertical) + (calc_badness grid false `Horizontal) in
-    printf "symmetry: %d\n" symmetry;
-    let sum' = sum + 1 in
-    sum_symmetry sum' grids'
-  ;;
-
 let () =
-  (* this should be a list of Arrays [][] *)
-  let data = get_data in
-  let first_grid = List.hd_exn data in
-  print_grid (first_grid);
-  printf "\n";
+  let grids = read_grids "../data/day-13" in
+  let total_sum = ref 0 in
+  List.iter grids ~f:(fun grid ->
+    let ans = (calc_badness grid false `Vertical) + (calc_badness grid false `Horizontal) in
+    total_sum := !total_sum + ans;
+  );
+  Printf.printf "13.1: %d\n" !total_sum;
 
-  (* List.iter data ~f:(fun grid -> print_grid grid; printf "\n"); *)
+  let total_sum = ref 0 in
+  List.iter grids ~f:(fun grid ->
+    let ans = (calc_badness grid true `Vertical) + (calc_badness grid true `Horizontal) in
+    total_sum := !total_sum + ans;
+  );
+  Printf.printf "13.2: %d\n" !total_sum;
 
-let first_puzzle = 
-  match sum_symmetry 0 data with
-  | 0 -> "You're Lost\n"
-  | combos -> Printf.sprintf "13.1: %d\n" combos 
-in
-  Printf.printf "%s" first_puzzle
-;;
   (*
   dune build puzzle_13_1.exe && cat ../data/day-13-test | dune exec ./puzzle_13_1.exe  
   *)
